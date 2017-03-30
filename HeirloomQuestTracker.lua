35e10fd731f0b305116511f76e1e039ef4346b84
lownignitus <lownignitus@gmail.com>
2017-03-30 04:25:58 -0700

    TOC update for 7.2

--- HeirloomQuestTracker.lua
+++ HeirloomQuestTracker.lua
@@ -1,6 +1,6 @@
 -- Title: Heirloom Quest Tracker
 -- Author: LownIgnitus
--- Version: 1.0.5
+-- Version: 1.0.6
 -- Desc: Addon to track Misprinted Coins, Quest completions for free heiloom upgrades, and 1st heroic end boss kill of the day.
 
 -- Globals
